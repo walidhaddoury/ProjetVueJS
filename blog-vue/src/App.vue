9808b7d6a9e935ca5ef5dca950a55bfeb7029cce
alexandre boiseaux <alexbx2002@gmail.com>
2021-10-06 16:34:17 +0200

    Article

--- blog-vue/src/App.vue
+++ blog-vue/src/App.vue
@@ -1,7 +1,11 @@
 <template>
-  
-  <div style="width: 100%;">
-    <ListArticle/>
+  <div id="app">
+    <div id="nav">
+      <router-link to="/">Home</router-link> |
+      <router-link to="/about">About</router-link> |
+      <router-link to="/article">Article</router-link>
+    </div>
+    <router-view/>
   </div>
 </template>
 
