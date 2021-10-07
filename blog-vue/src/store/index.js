bfe1e7f0c831343b3516f71febca1704f3803ea0
Walid <walidhaddoury@outlook.fr>
2021-10-07 14:06:52 +0200

    article de kilian sur VueJS

--- blog-vue/src/store/index.js
+++ blog-vue/src/store/index.js
@@ -119,11 +119,11 @@ export default new Vuex.Store({
         Auteur: "Marcel Prout",
       },
       {
-        Titre: "15",
+        Titre: "VueJS",
         Content:
-          "Overwatch est un jeu vidéo de tir (FPS) en ligne et en équipe de 6, développé et publié par Blizzard Entertainment. Le jeu est annoncé le 7 novembre 2014 à la BlizzCon, et est commercialisé le 24 mai 2016 sur Windows, PlayStation 4 et Xbox One et le 15 octobre 2019 sur Nintendo Switch. Le jeu met l'accent sur la coopération entre différentes classes représentées par différents personnages ayant chacun leurs capacités et particularités. Le jeu s'inspire notamment des jeux de tir en vue subjective en équipe de la décennie précédente mettant eux aussi l'accent sur la coopération entre plusieurs classes de personnage, notamment Team Fortress 2. Le 1er novembre 2019, Blizzard annonce la production de Overwatch 21, lors de cette BlizzCon. Le développement de ce second opus reste à suivre.",
-        Date: "13/07/2002",
-        Auteur: "Marcel Proust",
+          "Vue est un framework évolutif pour construire des interfaces utilisateur À la différence des autres frameworks monolithiques, Vue a été conçu et pensé pour pouvoir être adopté de manière incrémentale. Le cœur de la bibliothèque se concentre uniquement sur la partie vue, et il est vraiment simple de l’intégrer avec d’autres bibliothèques ou projets existants. D’un autre côté, Vue est tout à fait capable de faire tourner des applications web monopages quand il est couplé avec des outils modernes et des bibliothèques complémentaires.\n ",
+        Date: "07/10/2021",
+        Auteur: "Groupe 2",
       },
     ],
   },
